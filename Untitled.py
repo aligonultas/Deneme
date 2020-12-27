b889b22291cde81b7330780d32ae218112748572
aligonultas <74311339+aligonultas@users.noreply.github.com>
2020-12-27 18:18:46 +0300

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+isim=str(input("İsminiz:"))
+yas=int(input("Yaşınız:"))
+kalan=100-yas
+yıl=2020+kalan
+print("100.yaşınıza geldiğiniz yıl-->",yıl)
+
+
+# In[ ]:
+
+
+
+
